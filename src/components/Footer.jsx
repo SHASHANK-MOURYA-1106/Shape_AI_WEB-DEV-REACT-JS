d480f7b76ef691138dfe47c9f41c2990e008f6a7
SHASHANK-MOURYA-1106 <69301612+SHASHANK-MOURYA-1106@users.noreply.github.com>
2021-06-24 22:08:12 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>copyright shape_ai @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
